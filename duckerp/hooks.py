d6e031955aa0982b4d51942312563cd65d8dc093
Jilani S <jilani.blend@gmail.com>
2024-03-27 21:14:33 +0530

    Update hooks.py
    Add Chnages from Vivke

--- duckerp/hooks.py
+++ duckerp/hooks.py
@@ -28,6 +28,9 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
+doctype_js = {"Quotation" : "/duckerp/utils/quotation.js",
+			"Item":"/duckerp/utils/item.js",
+"Sales Order": "/duckerp/utils/sales_order.js"}
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
@@ -119,7 +122,11 @@ app_license = "mit"
 # }
 
 # Document Events
-# ---------------
+doc_events = {
+	"Item": {
+		"validate": "duckerp.duckerp.utils.item.weight_calculate",
+	}
+}
 # Hook on document methods and events
 
 # doc_events = {
