19db1b0c4a54286b826f543dab9baec1565c1881
Jilani S <jilani.blend@gmail.com>
2024-03-30 18:30:29 +0530

    Update item.js

--- duckerp/duckerp/utils/item.js
+++ duckerp/duckerp/utils/item.js
@@ -1,14 +1,1 @@
-frappe.ui.form.on("Item Raw Materials", {
-    custom_calculate_weight_kg:function(frm, cdt, cdn){
-        let row = locals[cdt][cdn];
-        frappe.call({
-            method: "duckerp.duckerp.utils.item.weight_kg",
-            args: { doc:frm.doc },
-            callback: function(r) {
-                if (r.message) {
-                    frappe.model.set_value(cdt, cdn, "weight_in_kg", r.message);
-                }
-            }
-        });
-    }
-})
+
